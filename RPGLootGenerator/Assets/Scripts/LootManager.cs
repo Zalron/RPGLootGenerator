31ffb6c5d605339b85e46e2b04d18148f0c7c6b9
Zalron <33308793+Zalron@users.noreply.github.com>
2019-03-15 20:48:17 +1100

    Added Lootmanager script

---
+++ RPGLootGenerator/Assets/Scripts/LootManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace
+public class LootManager : MonoBehaviour
+{
+    
+    void Start() // Start is called before the first frame update
+    {
+        
+    }
+
+    
+    void Update() // Update is called once per frame
+    {
+        
+    }
+}
