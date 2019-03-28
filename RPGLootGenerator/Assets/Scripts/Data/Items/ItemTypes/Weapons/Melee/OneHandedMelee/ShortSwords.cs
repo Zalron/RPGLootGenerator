ed1ffc697a8d1d1992e73d9d5aed6cf85f4e611f
Zalron <33308793+Zalron@users.noreply.github.com>
2019-03-28 15:18:54 +1100

    renamed all itemtypes to pural

--- RPGLootGenerator/Assets/Scripts/Data/Items/ItemTypes/Weapons/Melee/OneHandedMelee/ShortSwords.cs
+++ RPGLootGenerator/Assets/Scripts/Data/Items/ItemTypes/Weapons/Melee/OneHandedMelee/ShortSwords.cs
@@ -11,7 +11,7 @@ namespace RPGLootGenerator
         SteelGladius,
         ObsidianMaquahuitl,
     }
-    public static class ShortSword
+    public static class ShortSwords
     {
         static ShortSwordNames shortSwordNames;
         public static string GenerateShortSwordNames()
